1aa2e62b8797e8c86ae0c5149394ead112e57f9f
yeni <halukbilgin.uk@gmail.com>
2022-04-29 19:26:22 +0300

    theEnd

--- src/lambdaTutorial/Lambda04.java
+++ src/lambdaTutorial/Lambda04.java
@@ -1,9 +1,6 @@
 package lambdaTutorial;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.OptionalDouble;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Lambda04 {
@@ -14,14 +11,27 @@ public class Lambda04 {
                bolum (String)
                ogrcSayisi (int)
                notOrt (int)
-               olan POJO clas craete edip main method içinde 5 arklı obj'den List create ediniz.
+               olan POJO clas craete edip main method içinde 5 farklı obj'den List create ediniz.
      */
     public static void main(String[] args) {
 
+        Universite bogazici = new Universite("bogazici", "matematik", 571, 93);
+        Universite itu = new Universite("istanbul teknik", "matematik", 622, 81);
+        Universite istanbul = new Universite("istanbul", "hukuk", 1453, 71);
+        Universite marmara = new Universite("marmara", "bilgisayar muh", 1071, 77);
+        Universite ytu = new Universite("yıldız teknik", "gemi", 333, 74);
+        List<Universite> unv = new ArrayList<>(Arrays.asList(bogazici, itu, istanbul, marmara, ytu));
+
+        System.out.println(notOrt74BykUnv(unv));
     }
 
     //task 01--> notOrt'larinin 74' den buyuk oldg kontrol eden pr create ediniz.
+public static boolean notOrt74BykUnv(List<Universite> unv ){
 
+        return  unv.
+                stream().
+                allMatch(t->t.getNotOrt()>74);
+}
 
     //task 02-->ogrc sayilarinin   110 den az olmadigini  kontrol eden pr create ediniz.
 
@@ -54,3 +64,32 @@ public class Lambda04 {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
