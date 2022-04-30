ceb5ff32845ab9642dbde7442d1350d8bee70e58
yeni <halukbilgin.uk@gmail.com>
2022-04-30 19:24:28 +0300

    iyiBayramlar :)

--- src/lambdaTutorial/Lambda04.java
+++ src/lambdaTutorial/Lambda04.java
@@ -35,7 +35,22 @@ public class Lambda04 {
         System.out.println(notOrtBkSialiIlkUc(unv));
         System.out.println("\n   ***   ");
         System.out.println(enAzOgrcSaysi2Unv(unv));
+        System.out.println("\n   ***   ");
+        System.out.println("task 7: " + notOrt63BykUnvOgrcSayisiTopla(unv));
+        System.out.println("\n   ***   ");
+        System.out.println("mapToInt ile : " + notOrt63BykUnvOgrcSayisiToplaToInt(unv));
+        System.out.println("\n   ***   ");
+        System.out.println("task 8 : " + ogrcSayisi333BykNotOrtOrtlamaAl(unv));
+        System.out.println("\n   ***   ");
+        System.out.println("task 9 : " + mathBolmSayısı(unv));
+        System.out.println("\n   ***   ");
+        System.out.println("task 10 : " + ogrcSayisi571BykMaxNotort(unv));
+        System.out.println("\n   ***   ");
+        System.out.println("task 11 : " +ogrcSayisi1071AzMinnotOrt( unv));
+        System.out.println("\n   ***   ");
+
     }
+
 
     //task 01--> notOrt'larinin 74' den buyuk oldg kontrol eden pr create ediniz.
     public static boolean notOrt74BykUnv(List<Universite> unv) {
@@ -88,8 +103,8 @@ public class Lambda04 {
 
     //task 06--> ogrc sayisi en az olan 2. universite'yi  print ediniz.
 
-    public static List<Universite>  enAzOgrcSaysi2Unv(List<Universite> unv) {
-       return unv.
+    public static List<Universite> enAzOgrcSaysi2Unv(List<Universite> unv) {
+        return unv.
                 stream().
                 sorted(Comparator.comparing(Universite::getOgrSayisi)).
                 limit(2).
@@ -99,27 +114,70 @@ public class Lambda04 {
     }
     //task 07--> notOrt 63 'den buyuk olan universite'lerin ogrc sayilarini toplamini print ediniz.
 
-public static int notOrt63BykUnvOgrcSayisiTopla(List<Universite> unv){
-      return   unv.
+    public static int notOrt63BykUnvOgrcSayisiTopla(List<Universite> unv) {
+        return unv.
                 stream().
-                filter(t->t.getNotOrt()>63).
-                map(t->t.getOgrSayisi()).
-               // reduce(Integer::sum);
-               // reduce(Math::addExact);
-                reduce(0,(t,u)->t+u);
+                filter(t -> t.getNotOrt() > 63).
+                map(t -> t.getOgrSayisi()).
+                // reduce(Integer::sum);
+                // reduce(Math::addExact);
+                        reduce(0, (t, u) -> t + u);
 
-}
-    //task 08--> Ogrenci sayisi 130'dan buyuk olan universite'lerin notOrt'larinin ortalamasini bulunuz.
+    }
 
+    public static int notOrt63BykUnvOgrcSayisiToplaToInt(List<Universite> unv) {
+        return unv.
+                stream().
+                filter(t -> t.getNotOrt() > 63).
+                mapToInt(t -> t.getOgrSayisi()).
+                sum();
+        // mapToInt() --> bu method akısdaki elemanların data type'nı
+        // parameterisindeki() degere göre Int data type update eder
+
+    }
+
+    //task 08--> Ogrenci sayisi 333'dan buyuk olan universite'lerin notOrt'larinin ortalamasini bulunuz.
+    public static OptionalDouble ogrcSayisi333BykNotOrtOrtlamaAl(List<Universite> unv) {
+
+        return unv.
+                stream().
+                filter(t -> t.getOgrSayisi() > 333).
+                mapToDouble(t -> t.getNotOrt()).//
+                        average();//akısdakş elamnalrın ortlaması alındı
+        // mapToDouble() --> bu method akısdaki elemanların data type'nı
+        // parameterisindeki degere göre dooble data type update eder
+
+    }
 
     //task 09-->"matematik" bolumlerinin sayisini  print ediniz.
+    public static int mathBolmSayısı(List<Universite> unv) {
+        return (int) unv.
+                stream().
+                filter(t -> t.getBolum().contains("mat")).
+                count();//akısdaki elelman sayısını return eder
+        //count();-->akısdaki elelman sayısını return eder
 
+    }
 
-    //task 10-->Ogrenci sayilari 130'dan fazla olan universite'lerin en buyuk notOrt'unu bulunuz.
+    //task 10-->Ogrenci sayilari 571'dan fazla olan universite'lerin en buyuk notOrt'unu bulunuz.
+    public static OptionalInt ogrcSayisi571BykMaxNotort(List<Universite> unv) {
+        return unv.
+                stream().//akıs
+                        filter(t -> t.getOgrSayisi() > 571).//unv obj akısı fittrelendi
+                        mapToInt(t -> t.getNotOrt()).//akısdaki unv obj notOrt akısı olarak update edildi
+                        max();   //akısın en byk degerini return eder
 
+    }
 
-    //task 11-->Ogrenci sayilari 150'dan az olan universite'lerin en kucuk notOrt'unu bulunuz.
+    //task 11-->Ogrenci sayilari 1071'dan az olan universite'lerin en kucuk notOrt'unu bulunuz.
+    public static OptionalInt ogrcSayisi1071AzMinnotOrt(List<Universite> unv) {
+      return   unv.
+              stream().
+              filter(t->t.getOgrSayisi()<1071).
+              mapToInt(t->t.getNotOrt()).
+              min();
 
+    }
 
 }
 
