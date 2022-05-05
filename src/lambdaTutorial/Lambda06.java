fa0236cc72108fc05b7ea25a087e12debfa45d88
yeni <halukbilgin.uk@gmail.com>
2022-05-05 13:43:55 +0300

    start01

--- src/lambdaTutorial/Lambda06.java
+++ src/lambdaTutorial/Lambda06.java
@@ -1,5 +1,6 @@
 package lambdaTutorial;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -7,38 +8,51 @@ import java.util.stream.Collectors;
 
 public class Lambda06 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         //TASK 01 --> haluk.txt dosyasini okuyunuz.(Console'a yazdiriniz)
+        System.out.println("\n*** haluk.txt dosyasini okuyunuz -->  ");
 
 
         //TASK 02 --> haluk.txt dosyasini buyuk harflerle okuyunuz.(Console'a buyuk harflerle yazdiriniz)
+        System.out.println("\n*** haluk.txt dosyasini buyuk harflerle okuyunuz -->  ");
 
 
         //TASK 03 --> haluk.txt dosyasindaki ilk satiri kucuk harflerle yazdiriniz.
+        System.out.println("\n*** haluk.txt dosyasindaki ilk satiri kucuk harflerle okuyunuz 01 -->  ");
+
 
 
         //TASK 04 --> haluk.txt dosyasinda "basari" kelimesinin kac satirda gectiginiz yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda basari kelimesinin kac satirda gectiginiz yazdiriniz -->  ");
 
 
         //TASK 05 --> haluk.txt dosyasindaki farkli kelimeleri  yazdiriniz.
+        System.out.println("\n*** haluk.txt dosyasindaki farkli kelimeleri  yazdiriniz. -->  ");
 
 
         //TASK 06 --> haluk.txt dosyasindaki tum kelimeleri natural order  yazdiriniz.
+        System.out.println("\n*** haluk.txt dosyasindaki tum kelimeleri natural order  yazdiriniz. -->  ");
 
 
         //TASK 07 --> haluk.txt dosyasinda "basari" kelimesinin kac kere gectigini buyuk harf kucuk harf bagımsız yaziniz.
+        System.out.println("\n*** haluk.txt dosyasinda basari kelimesinin kac kere gectigini  yazdiriniz. -->  ");
 
 
         //TASK 08 --> haluk.txt dosyasinda "a" harfi gecen kelimelerin sayisini ekrana yazdiran programi yaziniz
+        System.out.println("\n*** haluk.txt dosyasinda a harfi gecen kelimelerin sayisini ekrana yazdiran programi yazdiriniz. -->  ");
 
 
         //TASK 09 --> haluk.txt dosyasinda icinde "a" harfi gecen kelimeleri yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda a harfi gecen kelimeler yazdiriniz. -->  ");
 
 
         //TASK 10 --> haluk.txt dosyasinda kac /farklı harf kullanildigini yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda kac /farklı harf kullanildigini  yazdiriniz. -->  ");
 
 
         //TASK 11 --> haluk.txt dosyasinda kac farkli kelime kullanildigini yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda kac farkli kelime kullanildigini  yazdiriniz. -->  ");
+
 
     }
 }
