d2b6bc7fa487dd0fbc7fce90cae396222438a1aa
yeni <halukbilgin.uk@gmail.com>
2022-05-05 19:54:40 +0300

    selametleGüzelİnsanlar :)

--- src/lambdaTutorial/Lambda06.java
+++ src/lambdaTutorial/Lambda06.java
@@ -2,24 +2,30 @@ package lambdaTutorial;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Lambda06 {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException {
         //TASK 01 --> haluk.txt dosyasini okuyunuz.(Console'a yazdiriniz)
         System.out.println("\n*** haluk.txt dosyasini okuyunuz -->  ");
-
+        Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).//Files class'dan lines() method call edilerek data
+                // çekilecek dosya yolu(path)
+                // paremetre girilerek path'deki dosyanın dataları akısa alındı
+                        forEach(System.out::println);//akısadaki datalar(her satırdaki string)yazıldı)
 
         //TASK 02 --> haluk.txt dosyasini buyuk harflerle okuyunuz.(Console'a buyuk harflerle yazdiriniz)
         System.out.println("\n*** haluk.txt dosyasini buyuk harflerle okuyunuz -->  ");
 
+        Files.lines(Paths.get("src/lambdaTutorial/haluk.txt")).
+                map(String::toUpperCase).//akısdaki datalar buyuk harfe update edldi
+                        forEach(System.out::println);//akısadaki datalar(her satırdaki string)yazıldı)
 
         //TASK 03 --> haluk.txt dosyasindaki ilk satiri kucuk harflerle yazdiriniz.
         System.out.println("\n*** haluk.txt dosyasindaki ilk satiri kucuk harflerle okuyunuz 01 -->  ");
-
 
 
         //TASK 04 --> haluk.txt dosyasinda "basari" kelimesinin kac satirda gectiginiz yazdiriniz
